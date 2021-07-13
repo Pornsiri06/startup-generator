b9c6b40f42216f175fd6aa8e685a6f8fc496866a
Pornsiri06 <pornsirisignsynod@gmail.com>
2021-07-13 20:08:03 +0700

    startup part 2 & add theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,101 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter_application_1/theme.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //final wordPair = WordPair.random();
+    // final wordPair = WordPair.random(); // Add this line.
     return MaterialApp(
-      title: 'Startup NamerV.1',
-      home: Randomwords(),
+      title: 'Startup namer',
+      home: RandomWords(),
+      theme: ThemeData(primaryColor: primaryTheme),
     );
   }
 }
 
-class Randomwords extends StatefulWidget {
-  const Randomwords({Key? key}) : super(key: key);
+class RandomWords extends StatefulWidget {
+  const RandomWords({ Key? key }) : super(key: key);
 
   @override
-  _RandomwordsState createState() => _RandomwordsState();
+  _RandomWordsState createState() => _RandomWordsState();
 }
 
-class _RandomwordsState extends State<Randomwords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18);
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];                 // NEW
+  final _saved = <WordPair>{};     // NEW
+  final _biggerFont = const TextStyle(fontSize: 18); // NEW
   @override
   Widget build(BuildContext context) {
-    //   final wordPair = WordPair.random();
-    //   return Text(wordPair.asSnakeCase);
-    return Scaffold(
+    //final wordPair = WordPair.random(); // Delete these... 
+    //return Text(wordPair.asPascalCase); // ... two lines.
+
+    return Scaffold (                     // Add from here... 
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
+        leading: Icon(Icons.access_alarm_outlined),
       ),
       body: _buildSuggestions(),
+    );                                      // ... to here.
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        // NEW lines from here...
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+                leading: Icon(
+                  Icons.favorite_border_outlined,
+                  color: Colors.red,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(context: context, tiles: tiles).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Name'),
+            ),
+            body: ListView(children: divided),
+          );
+        }, // ...to here.
+      ),
     );
   }
 
   Widget _buildSuggestions() {
     return ListView.builder(
-        padding: const EdgeInsets.all(16),
-        itemBuilder: (BuildContext _context, int i) {
-          if (i.isOdd) {
-            return Divider();
-          }
-          final int index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          return Divider();
+        }
+        final int index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);  // NEW
     return ListTile(
       leading: Icon(Icons.business_center_outlined),
       title: Text(
@@ -59,6 +103,19 @@ class _RandomwordsState extends State<Randomwords> {
         style: _biggerFont,
       ),
       subtitle: Text("Example"),
+      trailing: Icon(   // NEW from here... 
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {      // NEW lines from here...
+      setState(() {
+        if (alreadySaved) {
+          _saved.remove(pair);
+        } else { 
+          _saved.add(pair); 
+        } 
+      });
+    },  
     );
   }
 }
